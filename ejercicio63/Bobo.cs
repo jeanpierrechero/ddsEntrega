d98a68ba55c5b700121bc9653a36aa09dda10026
jean pierre <pierchero@gmail.com>
2020-05-06 20:22:20 -0300

    Ejercicio 63 - Juego Interactivo

---
+++ ejercicio63/Bobo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+
+using ejercicio63;
+namespace ejercicio63 {
+	public class Bobo : Tipo_Personaje {
+
+		public Bobo(){
+
+		}
+
+	}
+
+}
